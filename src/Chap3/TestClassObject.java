e0b844aece6756d140069ab5cac704039fc2374c
kasidit <kasiditza2546@gmail.com>
2023-11-25 17:05:58 +0700

    First commit!

--- src/Chap3/TestClassObject.java
+++ src/Chap3/TestClassObject.java
@@ -2,5 +2,12 @@ package Chap3;
 public class  TestClassObject{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
